b3fd4af3937caa0e68028b61bbb5789bb0906221
”名前” <>
2021-01-19 13:03:49 +0000

    php Laravel

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>profileの編集画面</h1>
-    </body>
-</html>
+@section('title', 'Profileの編集')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Profileの編集</h2>
+            </div>
+        </div>
+    </div>
+@endsection
