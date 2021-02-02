9fc61ab5773c6b095bf85665a93a34398a0ff2bc
”名前” <>
2021-02-02 13:02:47 +0000

    Laravel 13の課題

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::group(['prefix' => 'admin', 'middleware' =>'auth'],function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('profile/edit','Admin\ProfileController@update');
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 Route::get('XXX', 'AAAController@bbb');
