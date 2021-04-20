bf35f89db9ce00f3e7501bb40d15d0ebc60d14c9
谷 <mkt.kuv@icloud.com>
2021-04-20 13:18:20 +0000

    php laravel１７の途中

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('XXX', 'AAAController@bbb');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
